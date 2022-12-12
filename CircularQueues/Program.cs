a95b403c66376759eff1c708f27607a5d0bda801
Teuku Muhammad Attar Fadhilah <teuku.muhammad.ft21@mail.umy.ac.id>
2022-12-12 17:18:47 +0700

    add function for display

--- CircularQueues/Program.cs
+++ CircularQueues/Program.cs
@@ -81,6 +81,40 @@ namespace CircularQueues
         {
             int FRONT_position = FRONT;
             int REAR_position = REAR;
+            /* Checks if the queue is empty. */
+            if (FRONT == -1)
+            {
+                Console.WriteLine("Queue is empty\n");
+                return;
+            }
+            Console.WriteLine("\nElements in the queue are ................\n");
+            if (FRONT_position <= REAR_position)
+            {
+                /* traveres the queue till the last element present in an array. */
+                while (FRONT_position <= REAR_position)
+                {
+                    Console.Write(queue_array[FRONT_position] + "   ");
+                    FRONT_position++;
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                /* traverses the queue till the last position of the array.*/
+                while (FRONT_position <= max - 1)
+                {
+                    Console.Write(queue_array[FRONT_position] + "    ");
+                    FRONT_position++;
+                }
+                /* set the FRONT position to the first element of the array. */
+                FRONT_position = 0;
+                /* traverse the array till the last element present in the queue. */
+                while (FRONT_position <= REAR_position)
+                {
+                    Console.Write(queue_array[FRONT_position] + "    ");
+                    FRONT_position++;
+                }
+                Console.WriteLine();
             }
         }
     }
